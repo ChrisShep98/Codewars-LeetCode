1aa8c87f643df77498bff515e54076227f9abc2f
Chris Shepard <downstairsmac@Janes-iMac.local>
2022-04-25 20:35:36 -0400

    new codewars problem

--- 8 Kyu/main.js
+++ 8 Kyu/main.js
@@ -161,3 +161,9 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     }
   };
 
+// Array plus array
+function arrayPlusArray(arr1, arr2) {
+    let sumArr1 = arr1.reduce((preV, curV) => preV + curV, 0)
+    let sumArr2 = arr2.reduce((preV, curV) => preV + curV, 0) 
+    return sumArr1 + sumArr2
+  }
