4ea770051af04a226fc21f0c3ba32c30a29a6b74
Chris Shepard <Shepardchristopher1@gmail.com>
2022-05-09 20:26:54 -0400

    codewars solution

--- 8 Kyu/reversedwords.js
+++ 8 Kyu/reversedwords.js
@@ -3,7 +3,7 @@
 // Ex: "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The "
 
 function reverseWords(str){
-    return str.split(' ').reverse().join(' ')
+    return str.split(" ").reverse().join(" ")
   }
 
 
