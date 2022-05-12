8382fcae7fab389f85ea196aeaa4ee6b65487891
Chris Shepard <Shepardchristopher1@gmail.com>
2022-05-11 22:06:32 -0400

    codewars solution

--- 8 Kyu/main.js
+++ 8 Kyu/main.js
@@ -176,21 +176,7 @@ function removeChar(str){
   return str.substring(1, str.length - 1)
 };
 
-// Vowel Count
-function getCount(str) {
-  let count = 0
-  let arr = str.split('')
-  arr.forEach(element => {
-      if(element == 'a' ||
-      element == 'e' ||
-      element == 'i' ||
-      element == 'o' ||
-      element == 'u' ){
-          count++
-      }
-  });
-  return count
-}
+
 
 
 
