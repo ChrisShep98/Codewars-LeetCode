24dcf7814c1678b31bd4f7b389bc1a0dd7f409e8
Christopher Shepard <Shepardchristopher1@gmail.com>
2022-05-17 15:18:27 -0400

    new codewars solution

---
+++ 8 Kyu/nameShuffler.js
@@ -0,0 +1,7 @@
+// Name Shuffler
+// Write a function that returns a string in which firstname is swapped with last name.
+// Ex: nameShuffler('john McClane'); => "McClane john"
+
+const nameShuffler = str => str.split(' ').reverse().join('')
+
+
