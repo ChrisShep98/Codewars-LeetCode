a015127fb698ccccc4c6af0c8509520e4d86bed3
Christopher Shepard <Shepardchristopher1@gmail.com>
2022-06-03 17:34:13 -0400

    Credit Card Mask solution

--- 7 Kyu/creditCardMask.js
+++ 7 Kyu/creditCardMask.js
@@ -12,5 +12,4 @@
 // "Nananananananananananananananana Batman!" --> "####################################man!"
 
 // return masked string
-
 const maskify = cc => cc.slice(-4).padStart(cc.length, '#');
