c2371a98e01e819a76c443759c0a7c465b65e1cd
ChrisShep98 <shepardchristopher1@gmail.com>
2022-07-20 21:18:05 -0400

    Drink about solution

--- 8 Kyu/main.js
+++ 8 Kyu/main.js
@@ -57,16 +57,3 @@ function digitize(n) {
 
 // Jenny's secret message
 let jennyGreet = name => name === 'Johnny' ? "Hello, my love!" : "Hello, " + name + "!"
-
-// Drink about
-function peopleWithAgeDrink(age) {
-    if(age < 14){
-      return "drink toddy"
-    }else if(age < 18){
-      return "drink coke"
-    }else if(age < 21){
-      return "drink beer"
-    }else{
-      return "drink whisky"
-    }
-  };
