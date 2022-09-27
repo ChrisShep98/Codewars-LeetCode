6ed1b12e189119fa4cb52ff559a89b5cc3a85443
ChrisShep98 <shepardchristopher1@gmail.com>
2022-09-27 12:56:51 -0400

    new Vowel Count solution

--- 7 Kyu/vowelcount.js
+++ 7 Kyu/vowelcount.js
@@ -1,4 +1,10 @@
 // Vowel Count
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
+
+// Solution #1
+
 function getCount(str) {
     let count = 0
     let arr = str.split('')
@@ -13,3 +19,7 @@ function getCount(str) {
     });
     return count
   }
+
+// Solution #2
+
+const getCount = str => str.split('').filter(el => 'aeiou'.includes(el)).length;
