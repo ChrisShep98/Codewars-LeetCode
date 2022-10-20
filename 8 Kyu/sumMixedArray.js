a656f0bc8d55e24cf36a67d2a076058560283096
ChrisShep98 <shepardchristopher1@gmail.com>
2022-10-19 20:11:22 -0400

    New Sum Mixed Array solution

--- 8 Kyu/sumMixedArray.js
+++ 8 Kyu/sumMixedArray.js
@@ -2,6 +2,14 @@
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 //Return your answer as a number.
 
-function sumMix(x){
-    return x.map(element => Number(element)).reduce((preV, currV) => preV + currV, 0)
-  }
+// Solution #1
+
+function sumMix(x) {
+  return x
+    .map((element) => Number(element))
+    .reduce((preV, currV) => preV + currV, 0);
+}
+
+// Solution #2
+
+const sumMix = (x) => x.map((el) => +el).reduce((prev, curr) => prev + curr, 0);
