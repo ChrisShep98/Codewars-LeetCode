e4c16e897d60b0c2d96718b1e19ea16868a0d5e7
ChrisShep98 <shepardchristopher1@gmail.com>
2022-11-07 14:00:43 -0500

    Lost number in number sequence new solution

--- 7 Kyu/lostNumInNumSeq.js
+++ 7 Kyu/lostNumInNumSeq.js
@@ -19,6 +19,9 @@ function findDeletedNumber(arr, mixArr) {
     mixArr.reduce((prev, curr) => prev + curr, 0)
   );
 }
-console.log(findDeletedNumber([1, 2, 3, 4, 5], [3, 4, 1, 5]), 2);
-console.log(findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 9, 7, 4, 6, 2, 3, 8]),5);
-console.log(findDeletedNumber([1, 2, 3, 4, 5, 6, 7, 8, 9], [5, 7, 6, 9, 4, 8, 1, 2, 3]),0);
+
+// Solution #2
+
+function findDeletedNumber(arr, mixArr) {
+  return arr.filter((el) => mixArr.indexOf(el) == -1)[0] || 0;
+}
