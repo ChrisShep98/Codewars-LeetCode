cf453c630f9914edd0681ff526754cb2244dcb4e
ChrisShep98 <shepardchristopher1@gmail.com>
2022-11-15 18:36:01 -0500

    Sort the odd new solution

--- 6 Kyu/sortTheOdd.js
+++ 6 Kyu/sortTheOdd.js
@@ -6,7 +6,16 @@
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
+// Solution #1
+
 function sortArray(array) {
     const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
     return array.map((x) => x % 2 ? odd.shift() : x);
   }
+
+// Solution #2
+
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a,b) => b - a);
+  return array.map((x) => x % 2 ? odd.pop() : x);
+}
