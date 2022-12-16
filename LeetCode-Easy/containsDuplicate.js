59193cb003c0268a4fd4422ec1ebc084c4251b49
ChrisShep98 <shepardchristopher1@gmail.com>
2022-12-15 21:35:54 -0500

    Contains Duplicate solution

---
+++ LeetCode-Easy/containsDuplicate.js
@@ -0,0 +1,44 @@
+// 217. Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// Solution #1 (Set):
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const set = new Set(nums);
+  return set.size != nums.length;
+};
+
+// Solution #2:
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const hashTable = new Map();
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (hashTable.has(nums[i])) {
+      return true;
+    } else if (hashTable.set(nums[i], true));
+  }
+  return false;
+};
