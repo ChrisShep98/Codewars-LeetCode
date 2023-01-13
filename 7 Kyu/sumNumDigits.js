eb4dc2321250a23986c1d070c29e026113273298
ChrisShep98 <shepardchristopher1@gmail.com>
2023-01-12 20:01:53 -0500

    Summing a number's digits extra solution

--- 7 Kyu/sumNumDigits.js
+++ 7 Kyu/sumNumDigits.js
@@ -8,6 +8,19 @@
 // -32 --> 5
 // Let's assume that all numbers in the input will be integer values.
 
+// Solution #1
+
+function sumDigits(number) {
+  let sum = 0;
+  const numToArray = Math.abs(number).toString().split("");
+  for (let i = 0; i < numToArray.length; i++) {
+    sum += Number(numToArray[i]);
+  }
+  return sum;
+}
+
+// Solution #2
+
 function sumDigits(number) {
     return Math.abs(number).toString().split('').reduce((curr, prev) => curr+ + prev, 0);
   }
