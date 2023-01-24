5234c1930a3dcc9af29d9e4fcad3a975fbabe80f
ChrisShep98 <shepardchristopher1@gmail.com>
2023-01-23 22:11:47 -0500

    Divide and Conquer solution

--- 7 Kyu/divideAndConquer.js
+++ 7 Kyu/divideAndConquer.js
@@ -4,9 +4,22 @@
 
 // Return as a number.
 
+// Solution #1
+
 function divCon(x) {
   let numTotal = 0;
   let strTotal = 0;
   x.map((el) => (typeof el == "number" ? (numTotal += el) : (strTotal += +el)));
+  return numTotal - strTotal;
+}
+
+// Solution #2
+
+function divCon(x) {
+  let numTotal = 0;
+  let strTotal = 0;
+  for (let i = 0; i < x.length; i++) {
+    typeof x[i] == "string" ? (strTotal += Number(x[i])) : (numTotal += x[i]);
+  }
   return numTotal - strTotal;
 }
