d88905d42c9e7a495ea7a129124aeded642de530
ChrisShep98 <shepardchristopher1@gmail.com>
2025-01-27 16:36:34 -0500

    twoSum hashMap practice

---
+++ JavaScript/LeetCode/Repetition Practice/Arrays and Hashing/Easy/twoSum3.js
@@ -0,0 +1,44 @@
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// O(N^2) (Quadratic) solution:
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      console.log([map.get(nums[i]), i]);
+      return [map.get(nums[i]), i];
+    } else {
+      map.set(target - nums[i], i);
+    }
+  }
+};
+
+twoSum([2, 7, 11, 15], 9);
+// answer: [0, 1]
+
+twoSum([[3, 2, 4], 6]);
+// answer [1, 2]
